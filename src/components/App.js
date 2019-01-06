e0a7c4813a70c57b350adc64b4f15c3a6ca9aaeb
John Cameron <john.n.cameron@gmail.com>
2019-01-07 08:06:47 +1100

    connect unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
 
-const App = () => {
+class App extends Component {
+
+  
+  async onSearchSubmit(term) {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID f9e71158753f2160243979a54e97a908eb073f29c6200c9ffaf896dbea4ca793'
+        }
+      });
+      
+    console.log(response.data.results);
+      
+    }
+
+  render() {
     return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-            <div>Application</div>
-        </div>
-        
+      <div className="ui container" style={{ marginTop: '10px' }}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+          <div>Application</div>
+      </div>
     )
-};
+  }
+
+}
 
 export default App;
